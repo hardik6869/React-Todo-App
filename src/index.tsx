49e0213b0582f81579529b9ddfc5f07b8769dbee
hardik <hardik@gmail.com>
2022-06-16 17:05:33 +0530

    useState hook & Type alias

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 
 export default function App(): JSX.Element {
-  return <div>index</div>;
+  const [value, setValue] = useState<string>("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    setValue("");
+  };
+  return (
+    <Fragment>
+      <h1>Todo List </h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          required
+        />
+
+        <button type="submit"> Add Todo </button>
+        <h2>{value}</h2>
+      </form>
+    </Fragment>
+  );
 }
 
 const root = document.getElementById("root");
